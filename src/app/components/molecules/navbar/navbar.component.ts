ece1172fc5065883798d25f51f8c114d6659d449
biancamoreno <bianca.araujo@cognizant.com>
2020-06-26 15:41:49 -0300

    hotfix / style adjusts in components

--- src/app/components/molecules/navbar/navbar.component.ts
+++ src/app/components/molecules/navbar/navbar.component.ts
@@ -6,6 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  links = [
+    {
+      text: 'Sobre',
+      router: '/'
+    },
+    {
+      text: 'Pagar',
+      router: '/pagar'
+    }
+  ];
+
+  socials = [
+    {
+      alt: 'Linkedin',
+      href: 'https://www.linkedin.com/company/picpay',
+      src: 'https://picpay.com/static/images/ico-lk.png'
+    },
+    {
+      alt: 'Youtube',
+      href: 'https://www.youtube.com/user/PicPay',
+      src: 'https://picpay.com/static/images/ico-yt.png'
+    }
+  ];
+
   constructor() {}
 
   ngOnInit() {}
