8a43d11f6937d04ceb490765184f1dc68ecf8547
biancamoreno <bianca.araujo@cognizant.com>
2020-06-26 13:09:05 -0300

    feature / footer component and style adjusts in logo  component

--- src/app/components/atoms/logo/logo.component.ts
+++ src/app/components/atoms/logo/logo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-logo',
@@ -7,9 +8,21 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class LogoComponent implements OnInit {
   @Input() route: string;
+  @Input() textColor: string;
+  @Input() small: boolean;
+  @ViewChild('logo', { static: false }) logo: any;
+
   constructor() {}
 
   ngOnInit() {
     this.route ? this.route : '/';
+  }
+
+  ngAfterViewInit() {
+    this.changeStyle();
+  }
+
+  private changeStyle(color: string = '#0d9855') {
+    this.logo.nativeElement.style.fill = this.textColor ? this.textColor : color;
   }
 }
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,10 +6,11 @@ import { NavbarComponent } from './molecules/navbar/navbar.component';
 import { DirectivesModule } from '../directives/directives.module';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './organisms/header/header.component';
+import { FooterComponent } from './organisms/footer/footer.component';
 
 @NgModule({
-  declarations: [LogoComponent, ButtonComponent, NavbarComponent, HeaderComponent],
+  declarations: [LogoComponent, ButtonComponent, NavbarComponent, HeaderComponent, FooterComponent],
   imports: [CommonModule, DirectivesModule, RouterModule],
-  exports: [LogoComponent, ButtonComponent, NavbarComponent, HeaderComponent]
+  exports: [LogoComponent, ButtonComponent, NavbarComponent, HeaderComponent, FooterComponent]
 })
 export class ComponentsModule {}
