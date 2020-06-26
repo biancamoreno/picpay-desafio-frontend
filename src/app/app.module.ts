301cd2beb00d9d3fe51249e8f3e635370af0d3f8
biancamoreno <bianca.araujo@cognizant.com>
2020-06-26 11:14:35 -0300

    feature / link directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ComponentsModule } from 'src/app/components/components.module';
 import { AppComponent } from './app.component';
+import { LinkDirective } from './directives/link.directive';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LinkDirective],
   imports: [BrowserModule, ComponentsModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/directives/link.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[link]'
+})
+export class LinkDirective implements OnInit {
+  @Input() textColor: string;
+  constructor(private _elemento: ElementRef) {}
+
+  public ngOnInit() {
+    this.changeStyle();
+  }
+
+  private changeStyle(color: string = '#0d9855') {
+    this._elemento.nativeElement.style.color = this.textColor ? this.textColor : color;
+    this._elemento.nativeElement.style.textDecoration = 'underline';
+    this._elemento.nativeElement.style.cursor = 'pointerc';
+  }
+}
